b42a9d27f0104d0a4e6d8b869565d68a06b807e9
onlyzahid018-tech <onlyzahid018@gmail.com>
2026-05-02 14:47:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 app = FastAPI()
 
-# Supabase Credentials
-SUPABASE_URL = "https://pgvbntkzmevwrgzduxvu.supabase.co"
-SUPABASE_KEY = "sb_publishable_Fi0UIon1oVFJDFHbwukjBw_3fA662QL"
+# Render environment variables se keys read karega
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -17,9 +17,8 @@ def read_root():
 @app.get("/api/v1/matches")
 def get_matches():
     try:
-        # Matches table se data fetch karna
         response = supabase.table("Matches").select("*").execute()
         return {"status": "success", "data": response.data}
     except Exception as e:
         return {"status": "error", "message": str(e)}
-      
+        
